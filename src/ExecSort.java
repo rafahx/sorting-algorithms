51327ddd1a31613c13d61a1a7ff4f4d35e3f3fd1
Rafael Braz Viana <47992301+rafahx@users.noreply.github.com>
2022-03-15 12:23:07 -0300

    RadixSort added
    Radix sort added.
    Other little changes to make the font looks better (array declaration "[]" order)

--- src/ExecSort.java
+++ src/ExecSort.java
@@ -1,9 +1,10 @@
 import java.util.Random;
+import java.util.*;
 
 public class ExecSort {
 
     public static void main (String [] args) {
-        int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+        int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
         shuffleList(list);
         printList("ShuffledList: ", list);
@@ -27,11 +28,16 @@ public class ExecSort {
         quickSort(list, 0, list.length - 1);
         printList("QuickSort: ", list);
 
+        shuffleList(list);
+        printList("ShuffledList: ", list);
+        radixSort(list, list.length);
+        printList("RadixSort: ", list);
+
     }
 
     // Bubble Sort, onde o número maior chega ao final da lista mais rapidamente
     // Complexidade: Pior caso = O(n²); Melhor caso = O(1)
-    public static int[] bubbleSort(int list[]) {
+    public static int[] bubbleSort(int[] list) {
         boolean changed = true;
 
         // Enquanto houve mudança
@@ -55,7 +61,7 @@ public class ExecSort {
 
     // Ordena por inserção, assim como a organização de uma mão de cartas no baralho
     // Complexidade: Pior caso = O(n²); Melhor caso = O(n)
-    public static int[] insertionSort(int list[]) {
+    public static int[] insertionSort(int[] list) {
         int i, key;
 
         // Percorre o array a partir da segunda posição
@@ -80,7 +86,7 @@ public class ExecSort {
 
     // Ordena sempre buscando o valor menor do vetor e puxando para o menos índice
     // Complexidade: Pior caso = O(n²); Melhor caso = O(n²)
-    public static int[] selectionSort(int list[]) {
+    public static int[] selectionSort(int[] list) {
 
         for (int i = 0; i < list.length - 1; i++) {
             // Define o índice do menor valor
@@ -105,7 +111,7 @@ public class ExecSort {
 
     // Ordena a lista pelo método de divisão e conquista
     // Complexidade: Pior caso = O(n log n); Melhor caso = O(n log n) típico, O(n) variante natural
-    public static void mergeSort(int list[], int size){
+    public static void mergeSort(int[] list, int size){
         if (size < 2)
             return;
 
@@ -129,7 +135,7 @@ public class ExecSort {
     }
 
     // Faz a junção dos dois vetores ordenando eles dentro do vetor original
-    public static void merge(int leftList [],int rightList [], int list [],int leftSize, int rightSize){
+    public static void merge(int[] leftList, int[] rightList, int[] list, int leftSize, int rightSize){
         int i = 0, left = 0, right = 0;
 
         //Percorre enquanto não tiver passado por pelo menos o número total de índice de um dos vetores
@@ -152,7 +158,7 @@ public class ExecSort {
     // Também utiliza o método de divisão e consquista, define a ordenação da lista de acordo com o pivô escolhido
     // Não permite números duplicados
     // Complexidade: Pior caso = O(n²); Melhor caso = O(n log n)
-    public static void quickSort(int list[], int initial, int last) {
+    public static void quickSort(int[] list, int initial, int last) {
         if (initial < last){
             // Define o índice do pivô e faz a chamada recursiva da função para separar a lista em vetores menores
             int pivot = separate(list, initial, last);
@@ -162,7 +168,7 @@ public class ExecSort {
     }
 
     // Vai separar os números para esquerda ou direita dependendo se ele for menor ou maior que o pivô definido
-    public static int separate(int list[], int initial, int last) {
+    public static int separate(int[] list, int initial, int last) {
         // Define o pivô como sendo o primeiro número do vetor
         int pivot = list[initial];
         int i = initial + 1, l = last;
@@ -192,8 +198,61 @@ public class ExecSort {
         return l;
     }
 
+    // Ordenação que não utiliza comparação entre dois números para ordenar a lista
+    // Complexidade: Pior caso = O(nk); Melhor caso = -; k = comprimento médio da chave
+    static void radixSort(int[] list, int size) {
+        // Busca o maior número dentro da lista
+        int maxNumber = getMaxNumber(list, size);
+
+        // Utiliza o método CountSort para cada casa decimal do maior número da lista
+        for (int exponentialDivider = 1; maxNumber / exponentialDivider > 0; exponentialDivider *= 10) {
+            countSort(list, size, exponentialDivider);
+        }
+    }
+
+    // Realiza o CountSort para a casa decimal representada pelo divisor exponencial
+    static void countSort(int[] list, int size, int exponentialDivider) {
+        int[] listOutput = new int[size];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        // Cada índice do contador vai representar a quantidade de números equivalente
+        // Ex:
+        //      List:  { 3, 6, 6, 5, 2}
+        //      Count: { 0, 0, 1, 1, 0, 1, 2, 0, 0, 0}
+        for (int i = 0; i < size; i++)
+            count[(list[i] / exponentialDivider) % 10]++;
+
+        // Atualiza os contadores para saber quais números devem ir para cada índice
+        // Ex:
+        //      Count: { 0, 0, 1, 2, 2, 3, 5, 5, 5, 5}
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Monta a lista final
+        for (int i = size - 1; i >= 0; i--) {
+            // Para cada elemento da lista, verifica o número de acordo com o divisor exponencial, localiza ele dentro
+            // do array de contadores pelo índice, joga para o índice do novo array no índice - 1
+            listOutput[count[(list[i] / exponentialDivider) % 10] - 1] = list[i];
+            // Decrementa a quantidade de contadores daquele índice
+            count[(list[i] / exponentialDivider) % 10]--;
+        }
+
+        // Copia a lista output para a lista original
+        System.arraycopy(listOutput, 0, list, 0, size);
+    }
+
+    // Função adicional que busca o maior número dentro da lista
+    static int getMaxNumber(int[] list, int size) {
+        int maxNumber = list[0];
+        for (int i = 1; i < size; i++)
+            if (list[i] > maxNumber)
+                maxNumber = list[i];
+        return maxNumber;
+    }
+
     // Embaralha os números dentro da lista
-    public static int[] shuffleList(int list[]) {
+    public static void shuffleList(int[] list) {
         Random random = new Random();
         for(int i = list.length - 1; i > 0; i--){
             int j = random.nextInt(i + 1);
@@ -201,11 +260,10 @@ public class ExecSort {
             list[i] = list[j];
             list[j] = aux;
         }
-        return list;
     }
 
     // Printa a lista
-    public static void printList(String name, int list[]) {
+    public static void printList(String name, int[] list) {
         System.out.print("\n" + name + " \t");
         for (int i = 0; i <= list.length - 1; i++){ System.out.print(list[i] + " "); }
     }
